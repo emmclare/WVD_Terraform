f0f6742dbd1eac6171211a62d06e52695fa51e9e
Emily Mclaren <47635941+emmclare@users.noreply.github.com>
2021-05-20 11:28:59 +0100

    Update variables.tf
    changing hostpool name to use vars

--- variables.tf
+++ variables.tf
@@ -1,87 +1,113 @@
-variable "deploylocation" {
-  type        = string
-  default     = "West Europe"
-  description = "location"
+resource "time_rotating" "wvd_token" {
+  rotation_days = 30
 }
 
-variable "rgname"{
- type = string
- default = "WVD-TF"
- description = "resource group name"
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  features {}
 }
 
-variable "local_admin_username"{
- type = string
- default = "localadm"
- description = "admin username"
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = var.rgname
+  location = var.deploylocation
+}
+
+resource "azurerm_virtual_network" "VNet" {
+    name                = "WVD-TF-VNet"
+    address_space       = ["10.1.0.0/16"]
+    dns_servers         = ["10.0.0.4","168.63.129.16"]
+    location            = var.deploylocation
+    resource_group_name = azurerm_resource_group.resourcegroup.name
+    
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.resourcegroup.name
+  virtual_network_name = azurerm_virtual_network.VNet.name
+  address_prefixes     = ["10.1.0.0/24"]
+  
+}
+
+resource "azurerm_network_security_group" "NSG" {
+  name                = "WVD-TF-NSG "
+  location            = var.deploylocation
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 
-variable "admin_password"{
- type = string
- description = "admin password"
- 
+resource "azurerm_virtual_network_peering" "peer1" {
+  name                      = "peerwvdtftoad"
+  resource_group_name       = azurerm_resource_group.resourcegroup.name
+  virtual_network_name      = azurerm_virtual_network.VNet.name
+  remote_virtual_network_id = var.adVnetID
 }
 
-
-variable "rdsh_count" {
-  description = "**OPTIONAL**: Number of WVD machines to deploy"
-  default     = 2
+resource "azurerm_virtual_network_peering" "peer2" {
+  name                      = "peeradtowvdtf"
+  resource_group_name       = var.adRG
+  virtual_network_name      = var.adVnet
+  remote_virtual_network_id = azurerm_virtual_network.VNet.id
 }
 
-variable "host_pool_name" {
-  description = "Name of the RDS host pool"
-  default     = "WVD-TF-HP"
+resource "azurerm_virtual_desktop_workspace" "WS" {
+  name                = "workspace"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  friendly_name = "WVD TF Workspace"
+  description   = "WVD TF Workspace"
 }
 
-variable "vm_prefix" {
-  description = "Prefix of the name of the WVD machine(s)"
+resource "azurerm_virtual_desktop_host_pool" "HP" {
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  name                     = var.host_pool_name
+  friendly_name            = var.host_pool_name
+  validate_environment     = true
+  custom_rdp_properties    = "audiocapturemode:i:1;audiomode:i:0;"
+  description              = "WVD-TF-HP Terraform HostPool"
+  type                     = "Pooled"
+  maximum_sessions_allowed = 50
+  load_balancer_type       = "DepthFirst"
+  registration_info {
+    expiration_date = time_rotating.wvd_token.rotation_rfc3339
+
+  }
 }
 
-
-
-variable "domain_name" {
-  type = string
-  description = "Name of the domain to join"
-
+resource "azurerm_virtual_desktop_application_group" "remoteapp" {
+  name                = "wvd-tf-remoteapp"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  type                = "RemoteApp"
+  host_pool_id        = azurerm_virtual_desktop_host_pool.HP.id
 }
 
-variable "domain_user_upn" {
-  type = string
-  description = "UPN of the user to authenticate with the domain"
- 
+resource "azurerm_virtual_desktop_workspace_application_group_association" "ws-ra" {
+  workspace_id         = azurerm_virtual_desktop_workspace.WS.id
+  application_group_id = azurerm_virtual_desktop_application_group.remoteapp.id
 }
 
-variable "domain_password" {
-  type = string
-  description = "Password of the user to authenticate with the domain"
- 
-}
-
-
-variable "vm_size" {
-  description = "Size of the machine to deploy"
-  default     = "Standard_F2s"
-}
-
-variable "ou_path" {
-  default = ""
-
-}
-
-variable "adVnet"{
- type = string
- default = "adVNET"
- description = "resource group name"
-}
-
-variable "adRG"{
- type = string
- default = "RG-WVD-Internal"
- description = "resource group name for AD VM"
-}
-
-variable "adVnetID"{
- type = string
-  description = "resource id for vnet"
-}
