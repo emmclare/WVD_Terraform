3f2c6b0647962191eb22bf108fc0d3d4cf7077fa
Emily Mclaren <47635941+emmclare@users.noreply.github.com>
2021-06-21 13:29:59 +0100

    Update infrastructure.tf
    adding subnet/nsg association which I'd forgotten!

--- infrastructure.tf
+++ infrastructure.tf
@@ -44,16 +44,21 @@ resource "azurerm_network_security_group" "NSG" {
   resource_group_name = azurerm_resource_group.resourcegroup.name
 
   security_rule {
-    name                       = "SSH"
+    name                       = "HTTPS"
     priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "22"
+    destination_port_range     = "443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.NSG.id
 }
 
 
